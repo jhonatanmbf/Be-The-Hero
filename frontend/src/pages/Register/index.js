0ab1fb752ac6178b4cfe15b32a6e8790c4caab9e
jhonatanmbf <jmbf.jhonatan@gmail.com>
2020-06-05 00:53:04 -0300

    Front-end do app

---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,105 @@
+import React, {  useState   } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+import api from '../../services/api'
+import {  FiArrowLeft  } from 'react-icons/fi'
+import './styles.css'
+import logoImg from '../../assets/logo.svg'
+
+export default function Register(){
+    const [name, setName]= useState('')
+    const [email, setEmail]= useState('')
+    const [whatsapp, setWhatsapp]= useState('')
+    const [city, setCity]= useState('')
+    const [uf, setUF]= useState('')
+
+    const history = useHistory()
+
+
+    async function handleRegister(event){
+        event.preventDefault()
+        const data= {
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        }
+        try{
+            const response = await api.post('ongs', data)
+
+            alert(`Seu ID de acesso: ${response.data.id}`)
+            history.push('/')
+        }catch(err){
+            alert(`Erro no cadastro da ONG, tente novamente.`)
+
+        }
+
+
+    }
+    return(
+        <div className="register-container">
+
+            <div className="content">
+
+                <section>
+
+                    <img src={logoImg} alt="Be The Hero"/>
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
+
+                    <Link className="back-link"to="/">
+                        <FiArrowLeft  size={16} color='#E02041'/>
+                        Não tenho cadastro
+                    </Link>
+
+
+                </section>
+                <form onSubmit={handleRegister}>
+
+                    <input 
+                        placeholder="Nome da ONG"
+                        value={name}
+                        onChange={event => setName(event.target.value)}
+                        required="required"
+                    />
+                    <input 
+                        type="email" 
+                        placeholder="E-mail"
+                        value={email}
+                        onChange={event => setEmail(event.target.value)}
+                        required="required"
+                    />
+                    <input 
+                        placeholder="Whatsapp"
+                        value={whatsapp}
+                        onChange={event => setWhatsapp(event.target.value)}
+                        required="required"
+                    />
+
+                    <div className="input-group">
+                        <input 
+                            placeholder="Cidade"
+                            value={city}
+                            onChange={event => setCity(event.target.value)}
+                            required="required"
+                        />
+                        <input 
+                            placeholder="UF" 
+                            style={{width:80 }}
+                            value={uf}
+                            onChange={event => setUF(event.target.value)}
+                            required="required"
+                            
+                        />
+                    </div>
+
+                    <button className="button" type="submit">Cadastrar</button>
+
+                </form>
+
+
+            </div>
+
+        </div>
+    )
+}
